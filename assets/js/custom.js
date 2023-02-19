cd0e8237c887826125a60c35c2e8c145b76822aa
Eli <elibrumley@gmail.com>
2023-02-19 00:27:38 -0600

    sticky nav drop

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,25 @@
-// Check for dark mode preference at the OS level
-const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+// const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-// Get the body element
-const body = document.body;
+// const body = document.body;
 
-// Add the 'dark-mode' class to the body if the OS is in dark mode, otherwise add the 'light-mode' class
-if (isDarkMode) {
-  body.classList.add('dark-mode');
-} else {
-  body.classList.add('light-mode');
-}
+// if (isDarkMode) {
+//   body.classList.add('dark-mode');
+// } else {
+//   body.classList.add('light-mode');
+// }
+
+
+// select the body element
+const body = document.querySelector('body');
+
+// add event listener to window object for 'scroll' event
+window.addEventListener('scroll', () => {
+  // check if user has scrolled down 100px
+  if (window.scrollY > 100) {
+    // add the 'scrolled' class to the body element
+    body.classList.add('scrolled');
+  } else {
+    // remove the 'scrolled' class from the body element
+    body.classList.remove('scrolled');
+  }
+});
